30ff06b195f2181ada94b712a6ea17f9a1c10936
jmeza2003 <44911970+jmeza2003@users.noreply.github.com>
2020-01-31 14:58:00 -0800

    Add files via upload

---
+++ challenge_f/challenge_f.rb
@@ -0,0 +1,9 @@
+=begin
+
+YOUR TASK: Use your knowledge of varialbes and loops to change the
+"rate" of the banger.wav sample. Print to the Log to help you understand 
+what is happening and troubleshoot if you run into difficulty!
+
+=end
+
+c
